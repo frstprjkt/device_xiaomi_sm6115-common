d07fcc530c3d84b59a50de4f3c4cd4f235f00fc0
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-02-08 09:36:57 +0100

    sm6115-common: Setup namespace
    Change-Id: I97172190de6bcf572021b5a252a1dc587e1a3fc4

--- bengal.mk
+++ bengal.mk
@@ -12,3 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
