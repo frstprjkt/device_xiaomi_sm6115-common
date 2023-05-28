ed529d276a4cc499b47287e3e66913f67e6662cd
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-05-28 16:56:38 +0700

    sm6115-common: Build common Xiaomi sensors multihal
    Change-Id: I02c9250ed4754fbc93221a0878ae3b3b2352b9b1

--- bengal.mk
+++ bengal.mk
@@ -738,7 +738,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
