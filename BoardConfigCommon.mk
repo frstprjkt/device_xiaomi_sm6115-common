238776cf9080e0c6e21e28c5154ba828f2fd721a
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-05-28 11:37:48 +0700

    sm6115-common: Import common sepolicy
    [1] - Pull from Xiaomi SM8250 and SM6225
    
    Co-authored-by: Pig <pig.priv@gmail.com>
    Co-authored-by: Zinadin Zidan <zidan.roking@gmail.com>
    Co-authored-by: Utsav Balar <utsavbalar1231@gmail.com>
    Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>
    Co-authored-by: Pulkit077 <pulkitagarwal2k1@gmail.com>
    Co-authored-by: HeroBuxx <herobuxx@gmail.com>
    Co-authored-by: Jabiyeff <cebiyevanar@gmail.com>
    Co-authored-by: boedhack99 <boedhack@gmail.com>
    Co-authored-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Change-Id: Id09d9435519ee9b9316b8103f557f269947d3b7d
    Signed-off-by: Vayruz Rafli <vayruzrafli79@outlook.co.id>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -135,6 +135,8 @@ USE_SENSOR_MULTI_HAL := true
 
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Verified Boot
